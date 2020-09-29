eadb23d00fce1ed6f1744a5ddc40e613551d68e5
afiorentino <ajfioren@asu.edu>
2020-09-29 15:39:42 -0700

    initial commit. refs #27

---
+++ fetch-lafiancee.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.jsson.JSONObject;
+import org.json.JSONTokener;
+
+public class fetch-lafiancee {
+}
