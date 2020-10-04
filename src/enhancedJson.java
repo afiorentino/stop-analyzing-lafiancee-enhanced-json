6739276f15ffa897f4997dbfdeae7d1337c54ca7
Andrew Fiorentino <ajfioren@asu.edu>
2020-10-03 17:39:38 -0700

    created buid .xml

--- src/enhancedJson.java
+++ src/enhancedJson.java
@@ -4,6 +4,7 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
@@ -11,8 +12,15 @@ import java.io.Serializable;
 
 public class enhancedJson {
 	public static void main(String args[]){
-		FileInputStream in = new FileInputStream("lafiancee.json");
-		JSONObject obj = new JSONObject(new JSONTokener(in));
-		System.out.println(obj.get("urlPart"));
+		FileInputStream in;
+		/*try {
+			in = new FileInputStream("lafiancee.json");
+			JSONObject obj = new JSONObject(new JSONTokener(in));
+			System.out.println(JSONObject.getNames(obj));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}*/
+		System.out.println("will this work");
+		
 	}
 }
