9b472c3f6e2ddca0af5f7af27c7057ff35925c50
afiorentino <ajfioren@asu.edu>
2020-10-03 19:02:16 -0700

    scrapped last changes, parsing json file to find url part. #27

--- src/enhancedJson.java
+++ src/enhancedJson.java
@@ -1,26 +1,25 @@
-import org.json.JSONString;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Serializable;
-
+import org.json.*;
+import java.io.*;
+import java.util.*;
 
 public class enhancedJson {
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		FileInputStream in;
-		/*try {
+		try {
 			in = new FileInputStream("lafiancee.json");
 			JSONObject obj = new JSONObject(new JSONTokener(in));
-			System.out.println(JSONObject.getNames(obj));
+			JSONObject data = obj.getJSONObject("data");
+			JSONObject catalog = data.getJSONObject("catalog");
+			JSONObject category = catalog.getJSONObject("category");
+			JSONObject prodMetaData = category.getJSONObject("productsWithMetaData");
+			JSONArray list = prodMetaData.getJSONArray("list");
+			/*for(int i = 0; i < list.length(); i++) {
+				System.out.println(list.getJSONObject(i));
+			}*/
+			JSONObject sku = list.getJSONObject(0).getJSONObject("sku");
+			System.out.println(sku);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}*/
-		System.out.println("will this work");
-		
+		}
 	}
 }
